27d71b45597dde8d884b233421c3aa33b64e9806
Juergen Roi Bautista <96948355+NeverSkem@users.noreply.github.com>
2022-07-01 11:04:51 +0800

    Add files via upload

---
+++ Stack/InfixToPostfix/StackChar.java
@@ -0,0 +1,10 @@
+package Stack.InfixToPostfix;
+
+public interface StackChar{
+	public void push(char item);
+	public void pop();
+	public boolean isEmpty();
+	public int size();
+	public char getTop();
+	public int operator(char op);
+}
